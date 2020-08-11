25af3b7b818d6d3748afcd990e49dcc6551d2942
Amelie Kerne <amelie.kerne@gmail.com>
2020-08-11 16:56:38 +0200

    challenge 2 rails d2

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get "tasks", to: "tasks#index"
+  get "show/:id", to: "tasks#show", as: :show
+  get "new", to: "tasks#1"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
